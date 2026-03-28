460bf10e4fc0dc4fe759d1c38c4a041df07994cf
Sanket Surve <sanketsurve2756@gmail.com>
2026-03-28 11:39:00 +0530

    final deployment fixes

--- middleware.js
+++ middleware.js
@@ -1,14 +1,17 @@
 const Listing = require("./models/listing");
 const Review = require("./models/review");
+
+// LOGIN CHECK
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.redirectUrl = req.originalUrl;
-    req.flash("error", "You must be logged in to create listing!");
+    req.flash("error", "You must be logged in first!");
     return res.redirect("/login");
   }
   next();
 };
 
+// SAVE REDIRECT URL
 module.exports.saveRedirectUrl = (req, res, next) => {
   if (req.session.redirectUrl) {
     res.locals.redirectUrl = req.session.redirectUrl;
@@ -16,16 +19,18 @@ module.exports.saveRedirectUrl = (req, res, next) => {
   next();
 };
 
+// OWNER CHECK
 module.exports.isOwner = async (req, res, next) => {
   const { id } = req.params;
 
   const listing = await Listing.findById(id);
+
   if (!listing) {
     req.flash("error", "Listing not found!");
     return res.redirect("/listings");
   }
 
-  if (!listing.owner.equals(req.user._id)) {
+  if (!listing.owner || !listing.owner.equals(req.user._id)) {
     req.flash("error", "You don't have permission to do that!");
     return res.redirect(`/listings/${id}`);
   }
@@ -33,13 +38,21 @@ module.exports.isOwner = async (req, res, next) => {
   next();
 };
 
+// REVIEW AUTHOR CHECK
 module.exports.isReviewAuthor = async (req, res, next) => {
-  const {id, reviewId } = req.params;
+  const { id, reviewId } = req.params;
 
   const review = await Review.findById(reviewId);
-  if (!review.author.equals(res.locals.currUser._id)) {
-    req.flash("error", "You did not create these Review!");
+
+  if (!review) {
+    req.flash("error", "Review not found!");
     return res.redirect(`/listings/${id}`);
   }
+
+  if (!review.author || !review.author.equals(req.user._id)) {
+    req.flash("error", "You did not create this review!");
+    return res.redirect(`/listings/${id}`);
+  }
+
   next();
-}
+};
